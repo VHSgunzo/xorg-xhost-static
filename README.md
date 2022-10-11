6b0a068cd8f4b3df99126496398a0d1a680526b8
VHSgunzo <wosganzo@gmail.com>
2022-10-11 19:29:58 +0300

    v1.0.7.r3

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # xorg-xhost-static
-xorg-xhost compiled and packed with staticx
+* xorg-xhost compiled and packed with [staticx](https://github.com/JonathonReinhart/staticx)
+## To get started:
+* **Download the latest revision**
+```
+git clone https://github.com/VHSgunzo/xorg-xhost-static.git
+```
+* **Build**
+```
+cd xorg-xhost-static && ./build.sh
+```
---
+++ build.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+export MAKEFLAGS="-j$(nproc)"
+
+# WITH_UPX=1
+
+platform="$(uname -s)"
+platform_arch="$(uname -m)"
+
+if [ -x "$(which apt 2>/dev/null)" ]
+    then
+        apt update && apt install -y python3-pip patchelf \
+            autoconf git cmake upx-ucl libx11-dev xutils-dev libxmuu-dev \
+            build-essential clang pkg-config
+fi
+pip install staticx
+
+if [ -d build ]
+    then
+        echo "= removing previous build directory"
+        rm -rf build
+fi
+
+if [ -d release ]
+    then
+        echo "= removing previous release directory"
+        rm -rf release
+fi
+
+# create build and release directory
+mkdir build
+mkdir release
+pushd build
+
+# download xorg-xhost
+git clone https://github.com/freedesktop/xorg-xhost.git
+xorg_xhost_version="$(cd xorg-xhost && git describe --long --tags|sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g;s|xhost.||g')"
+mv xorg-xhost "xorg-xhost-${xorg_xhost_version}"
+echo "= downloading xorg-xhost v${xorg_xhost_version}"
+
+echo "= building xorg-xhost"
+pushd xorg-xhost-${xorg_xhost_version}
+./autogen.sh
+./configure
+make
+popd # xorg-xhost-${xorg_xhost_version}
+
+popd # build
+
+shopt -s extglob
+
+echo "= packaging xorg-xhost binary"
+if [[ "$WITH_UPX" == 1 && -x "$(which upx 2>/dev/null)" ]]
+    then
+        staticx --strip "build/xorg-xhost-${xorg_xhost_version}/xhost" release/xhost
+    else
+        staticx --no-compress --strip "build/xorg-xhost-${xorg_xhost_version}/xhost" release/xhost
+fi
+
+echo "= create release tar.xz"
+tar --xz -acf xorg-xhost-static-v${xorg_xhost_version}-${platform_arch}.tar.xz release
+# cp xorg-xhost-static-*.tar.xz ~/ 2>/dev/null
+
+if [ "$NO_CLEANUP" != 1 ]
+    then
+        echo "= cleanup"
+        rm -rf release build
+fi
+
+echo "= xorg-xhost v${xorg-xhost_version} done"
